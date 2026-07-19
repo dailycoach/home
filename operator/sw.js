1ea9e00dc394fca6d860d5e9a88986e7a9240bd1
dailycoach <ros2468@gmail.com>
2026-07-19 11:56:35 +0900

    feat: add 마음서재 운영실 service worker

---
+++ operator/sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'maeum-operator-v1';
+const ASSETS = ['/operator/', '/operator/manifest.webmanifest', '/operator/icon.svg'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request).then((cached) => cached || caches.match('/operator/')))
+  );
+});
