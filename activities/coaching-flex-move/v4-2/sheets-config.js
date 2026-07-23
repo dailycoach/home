4c19b4668c878bcbc3abf7c7836d9cfb4d5825b1
dailycoach <ros2468@gmail.com>
2026-07-23 23:49:28 +0900

    Activate anonymous Sheets collection endpoint

--- activities/coaching-flex-move/v4-2/sheets-config.js
+++ activities/coaching-flex-move/v4-2/sheets-config.js
@@ -1,8 +1,8 @@
 (()=>{
   'use strict';
   window.CFM_SHEETS_CONFIG = Object.freeze({
-    enabled: false,
-    endpoint: '',
+    enabled: true,
+    endpoint: 'https://script.google.com/macros/s/AKfycbwKi6SRfn8DhOsOfWdtiAF0RP-auSy5cJuo-cKa63gKk8CI2REnDXkZCgzkHKlieJor/exec',
     schemaVersion: '1.0',
     source: 'daily-coach-ing.com/coaching-flex-move'
   });
