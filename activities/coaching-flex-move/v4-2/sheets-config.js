98602637dff242643f9aca358d9be751242af5c2
dailycoach <ros2468@gmail.com>
2026-07-23 22:43:06 +0900

    Add disabled Sheets sync config

---
+++ activities/coaching-flex-move/v4-2/sheets-config.js
@@ -0,0 +1,9 @@
+(()=>{
+  'use strict';
+  window.CFM_SHEETS_CONFIG = Object.freeze({
+    enabled: false,
+    endpoint: '',
+    schemaVersion: '1.0',
+    source: 'daily-coach-ing.com/coaching-flex-move'
+  });
+})();
