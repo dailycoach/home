31b14a659235234cc0f4e7ff9499dd6e80babe8d
dailycoach <ros2468@gmail.com>
2026-07-26 01:13:56 +0900

    feat(academy): add access deployment configuration

---
+++ lcms/academy/access-config.js
@@ -0,0 +1,13 @@
+(() => {
+  'use strict';
+
+  window.RSEDU_ACADEMY_ACCESS = Object.freeze({
+    apiUrl: '',
+    courseId: 'lmc-lifetime-management-counselor',
+    entryPath: './enter.html',
+    defaultNext: './course.html?course=lmc-lifetime-management-counselor',
+    storageKey: 'rsedu-academy-access:v1',
+    requestTimeoutMs: 12000,
+    sessionHours: 12
+  });
+})();
